ca2e3616e0e5777a8a2a19175f2de7e82e7ffca4
Ibrahim <ibrachar68@gmail.com>
2023-01-12 14:24:49 +0100

    first commit

---
+++ todo-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+
+  }
+}
+</script>
+
+<template>
+
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ todo-vue/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+
+  }
+}
+
+</script>
+
+<template>
+  <p>ciao</p>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
