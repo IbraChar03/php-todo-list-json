e2df9a929c47283c793095df6e5940f89feb1fa8
Ibrahim <ibrachar68@gmail.com>
2023-01-13 18:22:11 +0100

    fine esercizio

--- todo-php/apiDeleteTask.php
+++ todo-php/apiDeleteTask.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: X-Requested-With");
 header("Content-Type: application/json");
 $id = $_GET["id"];
@@ -7,6 +7,7 @@ $list = $_GET["list"];
 var_dump($list);
 $jsonList = file_get_contents("results.json", true);
 $todoList = json_decode($jsonList);
-unset($todoList[$id]);
+// unset($todoList[$id]);
+array_splice($todoList, $id, 1);
 $jsonList = json_encode($todoList);
 file_put_contents("results.json", $jsonList);
